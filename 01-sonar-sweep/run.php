d0831c7f99abb33b8f44464bee1898e4ad65093f
Kae Verens <kae.verens@fieldmotion.com>
2021-12-04 11:36:42 +0000

    rename directories to be day-based

--- 01-sonar-sweep/run.php
+++
@@ -1,9 +0,0 @@
-<?php
-$inputs=file('input.txt');
-$increases=0;
-for ($i=1;$i<count($inputs);++$i) {
-	if (intval($inputs[$i])>intval($inputs[$i-1])) {
-		$increases++;
-	}
-}
-echo $increases."\n";
