8791f8de89708b727c2289c08e6eff8f0b500ee1
Kae Verens <kae.verens@fieldmotion.com>
2021-12-05 10:09:09 +0000

    solve day 5 test 1

---
+++ 05-01/run.php
@@ -0,0 +1,25 @@
+<?php
+$lines=file('input.txt');
+
+$points=[];
+
+foreach ($lines as $line) {
+	$ends=explode(' -> ', trim($line));
+	list($x1, $y1)=explode(',', $ends[0]);
+	list($x2, $y2)=explode(',', $ends[1]);
+	if ($x1!=$x2 && $y1==$y2) { // horizontal line
+		for ($x=min($x1, $x2); $x<=max($x1, $x2); ++$x) {
+			$points[$x.'_'.$y1]=($points[$x.'_'.$y1]??0)+1;
+		}
+	}
+	if ($y1!=$y2 && $x2==$x1) { // vertical line
+		for ($y=min($y1, $y2); $y<=max($y1, $y2); ++$y) {
+			$points[$x1.'_'.$y]=($points[$x1.'_'.$y]??0)+1;
+		}
+	}
+}
+$count=0;
+foreach ($points as $p) {
+	if ($p>1) $count++;
+}
+echo $count."\n";
